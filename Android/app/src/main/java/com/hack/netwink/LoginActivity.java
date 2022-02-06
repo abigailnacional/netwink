e840b24cdb6052cbd55656427bf2dbf01d71c85a
Venu Sai <venusaim23@gmail.com>
2022-02-06 17:23:32 +0530

    add create + home page layouts

--- Android/app/src/main/java/com/hack/netwink/LoginActivity.java
+++ Android/app/src/main/java/com/hack/netwink/LoginActivity.java
@@ -2,7 +2,9 @@ package com.hack.netwink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.hack.netwink.databinding.ActivityLoginBinding;
 
@@ -15,5 +17,19 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //login
+            }
+        });
+
+        binding.createAccTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, CreateAccount.class));
+            }
+        });
     }
 }
---
+++ Android/app/src/main/java/com/hack/netwink/Welcome.java
@@ -0,0 +1,24 @@
+package com.hack.netwink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //start
+                startActivity(new Intent(Welcome.this, HomePage.class));
+            }
+        }, 5000);
+    }
+}
