86b4ca33e0bf1fdecf92bdf570b1f8708bb2162a
Abigail Nacional <aan16@duke.edu>
2022-02-06 10:02:35 -0500

    Added again

---
+++ configFB.py
@@ -0,0 +1,10 @@
+config = {
+     "apiKey": "AIzaSyCm-za0dBaptEx6evvv3gTp9Pn7RUYN6e8",
+     "authDomain": "netwink.firebaseapp.com",
+     "projectId": "netwink",
+     "storageBucket": "netwink.appspot.com",
+     "messagingSenderId": "275178035488",
+     "appId": "1:275178035488:web:37bb9512aedf562e885f45",
+     "measurementId": "G-R3428S6FKW",
+     "databaseURL": "https://netwink-default-rtdb.firebaseio.com/"
+     } 
